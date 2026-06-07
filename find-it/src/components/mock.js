84fa8b948dd935313de07f354aaa36fe688d67c5
MuhdAbbasHamdard <muhdabbaskan@gmail.com>
2026-06-07 16:45:17 +0430

    create header component

---
+++ find-it/src/components/mock.js
@@ -0,0 +1,12 @@
+export const mainLinks = [
+  { name: "Home", path: "#" },
+  { name: "About us", path: "#" },
+  { name: "Properties", path: "#" },
+  { name: "Agents", path: "#" },
+  { name: "Blog", path: "#" },
+];
+
+export const actionLinks = [
+  { name: "Contact us", path: "#" },
+  { name: "Submit property", path: "#" },
+];
