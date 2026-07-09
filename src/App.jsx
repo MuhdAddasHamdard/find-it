ac738895679e3a16cc921884f1c0ccba1ee94f05
MuhdAddasHamdard <muhdabbaskan@gmail.com>
2026-07-09 10:21:11 +0430

    add design pattren to the code use barrel file to export all the components at once

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Featured from "./components/Featured";
-import Benefits from "./components/Benefits";
-import Locations from "./components/Locations/Locations";
-import Process from "./components/Process";
+import {
+  Header,
+  Hero,
+  Featured,
+  Benefits,
+  Locations,
+  Process,
+} from "./components";
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header";
+export { default as Hero } from "./Hero";
+export { default as Featured } from "./Featured";
+export { default as Benefits } from "./Benefits";
+export { default as Locations } from "./Locations/Locations";
+export { default as Process } from "./Process";
